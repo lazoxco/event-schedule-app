b395e06453875591131504f364476bc168f04ca3
github username <github email address>
2021-06-22 18:48:46 -0400

    Cleaned up code.

--- src/components/KeynoteSummary.js
+++ src/components/KeynoteSummary.js
@@ -3,9 +3,9 @@ const KeynoteSummary = ({ keynote }) => {
   return (
     <div className="card">
       <div className="card-body">
-        <h2 className="card-title">{ keynote.title }</h2>
-        <p className="card-text">by { keynote.speaker }</p> 
-        <p className="card-text">{ keynote.date_time }</p>
+        <h2 className="card-title">Topic: { keynote.title }</h2>
+        <p className="card-text">Presenter: { keynote.speaker }</p> 
+        <p className="card-text">Time: { keynote.date_time }</p>
         <a href="/" className="btn btn-primary">Learn More</a>
       </div>
     </div>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import HeroImage from '../components/HeroImage'
 import { connect } from 'react-redux'
+import Schedule from './Schedule'
 
 class Home extends Component {
   render(){
@@ -8,7 +9,21 @@ class Home extends Component {
       <div className="home">
         <HeroImage />
         <div className="container">
-          <h1>Home</h1>
+          <div className="row intro">
+            <div className="col">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/Lu3qzZ1Ck9g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className="col">
+              <h1>Eat. Sleep. Code. Repeat.</h1>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. </p>
+            </div>
+          </div> 
+          <div className="row">
+            <div className="col">
+              <h1 className="text-center">Conference Schedule</h1>
+              <Schedule />
+            </div>        
+          </div>
         </div>
       </div>
     )
